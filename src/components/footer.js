aaa9b0844fa7c3201c827d55204ac9e1e68f6e6f
Marlos Augusto <marlosaugusto@outlook.com>
2019-07-09 18:09:27 -0300

    acertos/refatoração das classes e add links

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,7 @@
 import { Link } from "gatsby";
-import PropTypes from "prop-types";
-import React, { useState } from "react";
+import React from "react";
 
-function Footer({ siteTitle }) {
-  // const [isExpanded, toggleExpansion] = useState(false);
+function Footer() {
 
   return (
     <div className="h-auto">
@@ -13,8 +11,8 @@ function Footer({ siteTitle }) {
             <div className="lg:w-357-74px lg:h-135px lg:pt-7px lg:pb-6-09px flex lg:content-between flex-wrap text-14 text-center lg:text-left">
               <h5 className="font-bold w-full lg:w-auto mb-20px lg:mb-0">Assine nossa Newsletter mensal</h5>
               <p className="font-display lg:w-350px leading-145 mb-20px lg:mb-0">Acompanhe-nos recebendo mensalmente novidades sobre o mercado de tecnologia no seu email.</p>
-              <form className="mx-auto inline-flex w-320px">
-                <input className="float-left w-214-8px h-40-91px mr-10px px-16px font-display text-cian-1 border border-black-8 rounded text-14" id="email_news" type="email" placeholder="Email"></input>
+              <form className="mx-auto lg:mx-0 inline-flex w-320px">
+                <input className="bg-white float-left w-214-8px h-40-91px mr-10px px-16px font-display text-cian-1 border border-black-8 rounded text-14" id="email_news" type="email" placeholder="Email"></input>
                 <button className="float-right w-85px lg:w-122-74px h-40-91px ml-10px bg-cian-1 hover:bg-cian-3 text-12 text-white font-semibold rounded" type="submit">
                   ASSINAR
                 </button>
@@ -24,20 +22,20 @@ function Footer({ siteTitle }) {
               <img className="mx-auto" src="/imgs/mimi.svg" alt="mimi codengage" />
             </div>
             <div className="w-full lg:w-auto justify-between inline-flex h-148px mt-29px lg:mt-0 lg:mb-0">
-              <div className="lg:w-161px lg:h-119px lg:mb-26px pt-3px xl:ml-88-4px">
-                <p className="text-14 font-display">+55 46 2605-0031</p>
-                <p className="text-14 font-display">+55 46 99985-8593</p>
-                <p className="text-14 font-display">contato@codengage.com</p>
-                <p className="text-14 font-display">comercial@codengage.com</p>
-                <p className="text-14 font-display">rh@codengage.com</p>
+              <div className="lg:w-161px lg:h-119px lg:mb-26px pt-3px xl:ml-88-4px text-14 font-display">
+                <p>+55 46 2605-0031</p>
+                <p>+55 46 99985-8593</p>
+                <p>contato@codengage.com</p>
+                <p>comercial@codengage.com</p>
+                <p>rh@codengage.com</p>
               </div>
-              <div className="w-82px lg:h-full lg:ml-40px xl:ml-67-53px flex content-between flex-wrap">
-                <a className="font-display text-14 text-cian-3" href="#empresa">‣ Empresa</a>
-                <a className="font-display text-14 text-cian-3" href="#servicos">‣ Serviços</a>
-                <a className="font-display text-14 text-cian-3" href="#clientes">‣ Clientes</a>
-                <a className="font-display text-14 text-cian-3" href="#testemunho">‣ Testemunho</a>
-                <a className="font-display text-14 text-cian-3" href="#talentos">‣ Talentos</a>
-                <a className="font-display text-14 text-cian-3" href="#contato">‣ Contato</a>
+              <div className="w-82px lg:h-full lg:ml-40px xl:ml-67-53px flex content-between flex-wrap font-display text-14 text-cian-3">
+                <Link className="inline-flex" to="/#empresa"><img src="icons/icon-play-cian.svg" alt="icon list"/>&nbsp;Empresa</Link>
+                <Link className="inline-flex" to="/#servicos"><img src="icons/icon-play-cian.svg" alt="icon list"/>&nbsp;Serviços</Link>
+                <Link className="inline-flex" to="/#clientes"><img src="icons/icon-play-cian.svg" alt="icon list"/>&nbsp;Clientes</Link>
+                <Link className="inline-flex" to="/#testemunho"><img src="icons/icon-play-cian.svg" alt="icon list"/>&nbsp;Testemunho</Link>
+                <Link className="inline-flex" to="/#talentos"><img src="icons/icon-play-cian.svg" alt="icon list"/>&nbsp;Talentos</Link>
+                <Link className="inline-flex" to="/#contato"><img src="icons/icon-play-cian.svg" alt="icon list"/>&nbsp;Contato</Link>
               </div>
             </div>
             
@@ -49,14 +47,14 @@ function Footer({ siteTitle }) {
               <img className="lg:w-7px h-7px" src="/icons/copyright-regular.svg" alt="copyright icon" />
               <p className="text-white text-10 font-display">&nbsp;Codengage 2019</p>
             </div>
-            <div className="inline-flex justify-between w-73px h-18px float-right mt-17px mb-15px">
-              <a href="/instagram">
+            <div className="relative inline-flex justify-between w-73px h-18px float-right mt-17px mb-15px">
+              <a href="https://www.instagram.com/codengagecom/" target="_blank">
                 <img src="/icons/instagram-brands-white.svg" alt="instagram icon" />
               </a>
-              <a href="/facebook">
+              <a href="https://www.facebook.com/codengagecom/" target="_blank">
                 <img src="/icons/facebook-square-brands-white.svg" alt="facebook icon" />
               </a>
-              <a href="/linkedin">
+              <a href="https://www.linkedin.com/company/codengage" target="_blank">
                 <img src="/icons/linkedin-brands-white.svg" alt="linkedin icon" />
               </a>
             </div>
