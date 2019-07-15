fd80924419dea821f77acf93e7b0c93211a52290
Marlos Augusto <marlosaugusto@outlook.com>
2019-07-15 17:48:34 -0300

    mailchimp code

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -1,3 +1,36 @@
 module.exports = (req, res) => {
   res.end('Welcome to Micro')
 }
+
+// let Mailchimp = require("mailchimp-api-v3");
+
+// let apiKey = process.env.REACT_APP_MAILCHIMP_API
+// let listId = process.env.REACT_APP_MAILCHIMP_LIST
+
+// let mailchimp = new Mailchimp(apiKey);
+
+// function postMessage(body) {
+//   console.log('postMessage', body)
+
+//   let callback = function (err, result) {
+//     if (err) {
+//       console.log('err:', err)
+//       return err
+//     }
+//     console.log('results', result)
+//     process.exit(0);
+//     return 200;
+//   }
+
+//   let add_new_member = {
+//     method: 'post',
+//     headers: {
+//       "Content-type": "application/json",
+//     },
+//     path: `/lists/${listId}/members`,
+//     body: body
+//   }
+//   mailchimp.request(add_new_member, callback)
+// }
+
+// export default postMessage;
