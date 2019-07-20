8fb6d431a76e8e3d106a625d8cfbd99813df0972
Marlos Augusto <marlosaugusto@outlook.com>
2019-07-19 21:08:25 -0300

    sending and updated data to mailchimp

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -1,4 +1,5 @@
 const { json, send } = require('micro');
+const md5 = require('md5');
 const microCors = require('micro-cors');
 const cors = microCors({ origin: ['http://localhost:8000'] })
 
@@ -9,34 +10,89 @@ dotenv.config();
 let apiKey = process.env.MAILCHIMP_API
 let listId = process.env.MAILCHIMP_LIST
 
-const endPoint = async (req, res) => {
-  let mailchimp = new Mailchimp(apiKey);
+const mailchimp = async (req, res) => {
+  let mc = new Mailchimp(apiKey);
   
   const body = await json(req);
+  let path = `/lists/${listId}/members/${md5(body.email_address)}`;
+  const originalTags = body.tags;
 
-  let add_new_member = {
-    method: 'POST',
+  let get_user = {
+    method: 'get',
     headers: {
       "Content-type": "application/json",
       "Accept": "application/json"
-
     },
-    path: `/lists/${listId}/members`,
+    path: path,
+  };
+
+  let manage_tags = {
+    method: 'post',
+    headers: {
+      "Content-type": "application/json",
+    },
+    path: `${path}/tags`,
+    body: { "tags": originalTags }
+  }
+
+  let create_or_update = {
+    method: 'put',
+    headers: {
+      "Content-type": "application/json",
+      "Accept": "application/json"
+    },
+    path: path,
     body: body
   };
 
+
+  const manageTags = async (request) => {
+    try {
+      const mTags = await mc.request(request);
+      console.log(mTags)
+  
+    } catch(e) {
+      console.log(e)
+      console.log(e.message)
+    }
+  }
+  
+  const registerOnMailchimp = async (request) => {
+    try {
+      body.tags = [];
+      const response = await mc.request(request);
+      console.log(response)
+      manageTags(manage_tags)
+
+      send(res, 200, {teste:123})
+    } catch(e) {
+      console.log(e.message)
+  
+      send(res, 400, { erro: e.message })
+    }
+  }
+
   try {
-    const response = await mailchimp.request(add_new_member);
+    const exist = await mc.request(get_user);
 
-    console.log(response)
-
-    send(res, 200, {teste:123})
+    if (exist.tags) {
+      exist.tags.map(({ name }) => {
+        if (name !== body.tags[0]) {
+          body.tags.push({
+            "name": name, "status": "active"
+          })
+          try {
+            manageTags(manage_tags);
+          } catch (e) {
+            console.log(e.message)
+          }
+        }
+      })
+    }
+    registerOnMailchimp(create_or_update);
   } catch(e) {
-    console.log(e)
-    console.log(e.message)
-
-    send(res, 400, { erro: e.message })
+    registerOnMailchimp(create_or_update);
   }
 }
 
-module.exports = cors(endPoint);
+module.exports = cors(mailchimp);
