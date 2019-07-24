ba82d4e04cf56fde1a07f7dd921bb41aaee0602e
Marlos Augusto <marlos.gomes@codengage.com>
2019-07-24 16:39:24 -0300

    footer.js with macro styles

--- packages/frontend/src/components/footer.js
+++ packages/frontend/src/components/footer.js
@@ -41,9 +41,9 @@ function Footer() {
   return (
     <div className="h-auto">
       <div className="h-auto pt-30px lg:pt-56px">
-        <div className="mx-auto container relative max-w-320px sm:max-w-424 lg:max-w-848 xl:max-w-1025">
+        <div className="container mx-auto relative max-w-320px sm:max-w-424 lg:max-w-848 xl:max-w-1025">
           <div className="w-auto mx-20px pb-203px lg:pb-54px lg:inline-flex lg:justify-between">
-            <div className="lg:w-357-74px lg:h-135px lg:pt-7px lg:pb-6-09px flex lg:content-between flex-wrap text-14 text-center lg:text-left">
+            <div className="lg:w-357-74px lg:h-135px lg:pt-7px lg:pb-6-09px flex lg:content-between flex-wrap text-14 txtc-lgtxtl">
               <h5 className="font-bold w-full lg:w-auto mb-20px lg:mb-0">Assine nossa Newsletter mensal</h5>
               <p className="font-display lg:w-350px leading-145 mb-20px lg:mb-0">Acompanhe-nos recebendo mensalmente novidades sobre o mercado de tecnologia no seu email.</p>
               <form className="mx-auto lg:mx-0 inline-flex w-320px">
@@ -64,7 +64,7 @@ function Footer() {
                 <p>comercial@codengage.com</p>
                 <p>rh@codengage.com</p>
               </div>
-              <div className="w-82px lg:h-full lg:ml-40px xl:ml-67-53px flex content-between flex-wrap font-display text-14 text-cian-3">
+              <div className="c-grid w-82px lg:h-full lg:ml-40px xl:ml-67-53px font-display text-14 text-cian-3">
                 <Link className="inline-flex" to="/#empresa"><img src="icons/icon-play-cian.svg" alt="icon list"/>&nbsp;Empresa</Link>
                 <Link className="inline-flex" to="/#servicos"><img src="icons/icon-play-cian.svg" alt="icon list"/>&nbsp;Serviços</Link>
                 <Link className="inline-flex" to="/#clientes"><img src="icons/icon-play-cian.svg" alt="icon list"/>&nbsp;Clientes</Link>
